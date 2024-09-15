f41697d40d61caaff524f597558954cc32f6e748
ID22663 <gayratyoldoshboyev3@gmail.com>
2024-09-15 15:18:00 +0500

    carisel file edit

--- src/components/CountryCarusel.jsx
+++ src/components/CountryCarusel.jsx
@@ -1,30 +1,46 @@
-import { Carousel } from "flowbite-react";
+import { Carousel, Flowbite } from "flowbite-react";
+import { getFromLocalStorage } from "../utils";
+
+
+const NumArray = (array, size) => {
+  const result = [];
+  for (let i = 0; i < array.length; i += size) {
+    result.push(array.slice(i, i + size));
+  }
+  return result;
+};
 
 function CountryCarusel() {
+  const countries = getFromLocalStorage('selectedCountries') || [];
+  const countriesNum = NumArray(countries, 4);
+
   const customTheme = {
-    carousel: {
-      root: {
-        base: "relative w-full h-full",
-      },
-      indicators: {
-        active: "bg-blue-600",
-        inactive: "bg-gray-200",
-      },
       control: {
-        base: "hidden", // Hide the control buttons (arrows)
-      },
-    },
+        base: " hidden",
+      }
   };
 
   return (
-    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-      <Carousel theme={customTheme} indicators={false}>
-        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
-      </Carousel>
+    <div>
+        <div className="relative h-56 sm:h-64 xl:h-80 2xl:h-96">
+          <Carousel indicators={false} theme={customTheme} >
+            {countriesNum.map((countrNum, index) => (
+              <div key={index} className="flex justify-around space-x-4">
+                {countrNum.map((country) => (
+                  <div key={country.cca2} className="flex flex-col items-center w-1/4 p-2">
+                    <img
+                      src={country.flags.svg}
+                      alt={`${country.name.common} flag`}
+                      className="w-28 h-16 object-cover"
+                    />
+                    <p className="mt-2 text-lg font-semibold">{country.name.common}</p>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </Carousel>
+        </div>
+   
     </div>
   );
 }
